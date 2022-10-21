5e156a43f19cfb68c5d6aadd872340546d980824
jolo-dev <johnyscrazy@gmail.com>
2022-10-21 21:05:04 +0200

    remove deprecated set-output

--- .github/workflows/helper.sh
+++ .github/workflows/helper.sh
@@ -9,13 +9,13 @@ PROD="main"
 
 
 if [ "$REF_NAME" = "$DEV" ]; then
-    echo "::set-output name=value::alpha1"
+    echo "value=alpha1" >> $GITHUB_OUTPUT
     echo "It's dev"
 elif [ "$REF_NAME" = "$TEST" ]; then
-    echo "::set-output name=value::beta1"
+    echo "value=beta1" >> $GITHUB_OUTPUT
     echo "It's test"
 elif [ "$REF_NAME" = "$QA" ]; then
-    echo "::set-output name=value::rc.1"
+    echo "value=rc.1" >> $GITHUB_OUTPUT
     echo "It's qa"
 elif [ "$REF_NAME" = "$PROD" ]; then
     echo "It's prod"
